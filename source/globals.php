c71efa276dbc1d0bd8177b8b3d447879f10c78e0
Leonardo Vilarinho <leonardo-i@outlook.com>
2017-12-06 01:32:12 -0200

    add lock with dependence

--- source/globals.php
+++ source/globals.php
@@ -9,7 +9,7 @@ if (!defined('APP_NAME')) {
 	  )  _   _ <fg=blue>  _ ( _   _ </> _)_ _   _
 	_(_ ) ) (  <fg=blue> )_) ) ) )_)</> (_ (_) )
 	        _) <fg=blue>(       (   </>
-	                                           v1.0.9
+	                                           v1.0.12
 	                            By <fg=blue>Leonardo Vilarinho</>'.PHP_EOL);
 }
 
