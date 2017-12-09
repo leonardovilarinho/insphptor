673bdec20278b34a1ea57d44e588a18900438b1d
Leonardo Vilarinho <leonardo-i@outlook.com>
2017-12-09 19:02:20 -0200

    change version for 15

--- source/globals.php
+++ source/globals.php
@@ -9,7 +9,7 @@ if (!defined('APP_NAME')) {
 	  )  _   _ <fg=blue>  _ ( _   _ </> _)_ _   _
 	_(_ ) ) (  <fg=blue> )_) ) ) )_)</> (_ (_) )
 	        _) <fg=blue>(       (   </>
-	                                           v1.0.14
+	                                           v1.0.15
 	                            By <fg=blue>Leonardo Vilarinho</>'.PHP_EOL);
 }
 
